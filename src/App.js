78a95486bc479961a8e536ab55e8951b81e7f0d2
Fabricio Trindade <fabrtrindade@gmail.com>
2022-05-18 19:25:03 -0300

    Creating first React component.

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
+function App() {
+  return (  
+    <section>
+      <form>
+        <input type="text" placeholder = "Título"/>
+        <textarea placeholder="Escreva sua nota...."/>
+        <button>Criar Nota</button>
+      </form>
+      <ListaDeNotas/>
+      <ListaDeNotas/>
+      <ListaDeNotas/>
+    </section>
+
+  );
+}
+
+export default App;
