fdb0c25ee3a456fe3c93d796e85a3cd4585bd137
FabTrindade <fabrtrindade@gmail.com>
2022-05-22 21:45:05 -0300

    Importing SVGs.

--- src/components/CardNota/CardNota.jsx
+++ src/components/CardNota/CardNota.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import {ReactComponent as DeleteForeverSVG} from  "../../assets/img/delete_forever.svg"
 import "./estilo.css";
 class CardNota extends Component {
   render() {
@@ -6,6 +7,7 @@ class CardNota extends Component {
       <section className="card-nota">
         <header className="card-nota_cabecalho">
           <h3 className="card-nota_titulo">{this.props.titulo}</h3>
+          <DeleteForeverSVG />
         </header>
         <p className="card-nota_texto">{this.props.texto}</p>
       </section>
